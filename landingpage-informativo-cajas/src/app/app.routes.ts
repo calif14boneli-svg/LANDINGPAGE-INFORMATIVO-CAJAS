ea468bc4832624e4fbf80f8d7b73a324966ea6dc
calif <calif14boneli@gmail.com>
2026-02-13 13:04:56 -0500

    feat: Initialize Angular application with core components, routing, and 'Principal' and 'Nosotros' pages.

--- landingpage-informativo-cajas/src/app/app.routes.ts
+++ landingpage-informativo-cajas/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Principal } from './paginas/principal/principal';
+import { NosotrosComponent } from './paginas/nosotros/nosotros';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    { path: '', component: Principal },
+    { path: 'nosotros', component: NosotrosComponent }
+]
---
+++ landingpage-informativo-cajas/src/app/paginas/principal/principal.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-principal',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './principal.html',
+  styleUrl: './principal.css',
+})
+export class Principal {
+
+}

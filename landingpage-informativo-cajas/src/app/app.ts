68ba2c4ee2dc72a0495bcafcc7ca2f6cf1075958
calif <calif14boneli@gmail.com>
2026-02-18 12:45:16 -0500

    EDICION: portada y barra de menu

--- landingpage-informativo-cajas/src/app/app.ts
+++ landingpage-informativo-cajas/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

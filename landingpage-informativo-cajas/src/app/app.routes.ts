5f7f4273a4fa0e2ab11b22d56e11cc248d57e3a6
JheysonHinojosa <jheysonhinojosarequejo@gmail.com>
2026-02-18 17:30:10 -0500

    MEJORANDO LA PESTAÑA DE "TIPOS PRODUCTO" "DETALLE PRODUCTO"

--- landingpage-informativo-cajas/src/app/app.routes.ts
+++ landingpage-informativo-cajas/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Principal } from './paginas/principal/principal';
 import { NosotrosComponent } from './paginas/nosotros/nosotros';
+import { TipoProducto } from './paginas/tipo-producto/tipo-producto';
+import { ProductoDetalle } from './paginas/producto-detalle/producto-detalle';
 
 
 export const routes: Routes = [
 
     { path: '', component: Principal },
-    { path: 'nosotros', component: NosotrosComponent }
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'tipoProducto', component: TipoProducto },
+{ path: 'tipoProducto/:id', component: ProductoDetalle },
+  { path: '**', redirectTo: '' }
 ]

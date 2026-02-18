2689f5c622fb0f4c597bd93340100f7b26b223a4
calif <calif14boneli@gmail.com>
2026-02-18 18:33:13 -0500

    EDIT: contactos

--- landingpage-informativo-cajas/src/app/app.routes.ts
+++ landingpage-informativo-cajas/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Principal } from './paginas/principal/principal';
 import { NosotrosComponent } from './paginas/nosotros/nosotros';
+import { Contacto } from './paginas/contacto/contacto';
 
 
 export const routes: Routes = [
 
     { path: '', component: Principal },
-    { path: 'nosotros', component: NosotrosComponent }
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'contacto', component: Contacto }
 ]

a76105a5b94598b81582d29f413f923b997eb422
calif <calif14boneli@gmail.com>
2026-02-19 17:42:40 -0500

    LISTO1

--- landingpage-informativo-cajas/src/app/app.routes.ts
+++ landingpage-informativo-cajas/src/app/app.routes.ts
@@ -1,10 +1,23 @@
 import { Routes } from '@angular/router';
 import { Principal } from './paginas/principal/principal';
 import { NosotrosComponent } from './paginas/nosotros/nosotros';
+import { Galeria } from './paginas/galeria/galeria';
+import { Contacto } from './paginas/contacto/contacto';
+import { ProductoDetalle } from './paginas/producto-detalle/producto-detalle';
+import { Servicios } from './paginas/servicios/servicios';
+import { ServicioDetalle } from './paginas/servicio-detalle/servicio-detalle';
+import { TipoProducto } from './paginas/tipo-producto/tipo-producto';
+
 
 
 export const routes: Routes = [
 
     { path: '', component: Principal },
-    { path: 'nosotros', component: NosotrosComponent }
+    { path: 'nosotros', component: NosotrosComponent },
+    { path: 'galeria', component: Galeria },
+    { path: 'contacto', component: Contacto },
+    { path: 'producto-detalle', component: ProductoDetalle },
+    { path: 'servicios', component: Servicios },
+    { path: 'servicio-detalle', component: ServicioDetalle },
+    { path: 'tipo-producto', component: TipoProducto }
 ]

2136ef57fac8db84a5d30293ed38e91f8392b183
calif <calif14boneli@gmail.com>
2026-02-19 16:29:08 -0500

    EDIT /GALERIA

--- landingpage-informativo-cajas/src/app/app.routes.ts
+++ landingpage-informativo-cajas/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Principal } from './paginas/principal/principal';
 import { NosotrosComponent } from './paginas/nosotros/nosotros';
 import { Contacto } from './paginas/contacto/contacto';
+import { Galeria } from './paginas/galeria/galeria';
 
 
 export const routes: Routes = [
 
     { path: '', component: Principal },
     { path: 'nosotros', component: NosotrosComponent },
-    { path: 'contacto', component: Contacto }
+    { path: 'contacto', component: Contacto },
+    { path: 'galeria', component: Galeria }
 ]
